aaae59e4605118e9a19fd8821d881b656abf66c7
rocktyy <tianyuyan@163.com>
2019-05-24 09:15:16 +0800

    Fix: 1.controller修改

--- server/app/controller/home.js
+++ server/app/controller/home.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const Controller = require('egg').Controller;
 class HomeController extends Controller {
 
   async index() {
--- server/config/config.default.js
+++ server/config/config.default.js
@@ -6,7 +6,6 @@ module.exports = appInfo => {
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_1536859564199_3953';
 
-
   // add your config here
   config.middleware = [];
 
