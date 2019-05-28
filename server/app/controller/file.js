59979de5cebb244a0a920dcfe2d45da44fd2af39
颜殊 <rock.tyy@alibaba-inc.com>
2019-05-28 14:03:18 +0800

    FIx: 1.线上服务器地址

--- server/app/controller/file.js
+++ server/app/controller/file.js
@@ -9,12 +9,7 @@ const sendToWormhole = require('stream-wormhole');
 // 此处设置为应用的静态资源目录，方便外界访问
 const UPLOAD_DIR  = 'app/public'; 
 
-const SERVER_DOMAIN =  {
-  'dev': 'http://127.0.0.1:7001',
-  'sit': 'https://app2138419400test.mapp-test.xyz',
-  'online':'https://kangaroo.kangarooaaction.com',
-}
-const DEMO_DOMAIN = SERVER_DOMAIN['online'];
+const DEMO_DOMAIN = 'https://kangaroo.kangarooaaction.com';
 
 class FileController extends Controller {
 
--- server/config/config.default.js
+++ server/config/config.default.js
@@ -1,23 +1,5 @@
 'use strict';
 
-const clientArray = [
-  {
-    host: '47.101.222.13', //test
-    port: '3306',
-    user: 'admin',
-    password: 'dMa4Ghgj', 
-    database: 'kangaroo',
-    charset : 'utf8mb4'
-  },{
-    host: '47.101.202.3', //online
-    port: '3306',
-    user: 'admin',
-    password: 'dMa4Ghgj', 
-    database: 'kangaroo',
-    charset : 'utf8mb4'
-  }
-] 
-
 module.exports = appInfo => {
   const config = (exports = {});
 
@@ -36,7 +18,14 @@ module.exports = appInfo => {
 
   // !!!!!!MySQL参数host（数据库服务IP地址）和password（admin用户的登录密码）需修改为小程序云应用服务中的MySQL的对应值!!!!!!
   config.mysql = {
-    client: clientArray[1],
+    client: {
+      host: '47.101.202.3', //online
+      port: '3306',
+      user: 'admin',
+      password: 'dMa4Ghgj', 
+      database: 'kangaroo',
+      charset : 'utf8mb4'
+    },
     app: true,
     agent: false,
   };
