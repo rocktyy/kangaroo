bb5861cbf20cd9dcf9532021504a92cb80dd05eb
颜殊 <rock.tyy@alibaba-inc.com>
2019-05-30 18:28:25 +0800

    Fix: 1.小程序线上域名

--- client/app.js
+++ client/app.js
@@ -8,7 +8,7 @@ App({
   startDate: '2019/6/01 8:00',
 
   // 请修改为您的小程序云应用的真实域名
-  demoDomain: 'https://kangaroo.kangarooaaction.com',
+  demoDomain: 'https://kangarooaaction.autovideogroup.com',
 
   getUserInfo() {
     var theDemoDomain = this.demoDomain;
