60829db9c11c737402caaa85b4146191ee07e1ac
颜殊 <rock.tyy@alibaba-inc.com>
2019-05-30 14:56:52 +0800

    no message

--- server/config/config.default.js
+++ server/config/config.default.js
@@ -19,7 +19,7 @@ module.exports = appInfo => {
   // !!!!!!MySQL参数host（数据库服务IP地址）和password（admin用户的登录密码）需修改为小程序云应用服务中的MySQL的对应值!!!!!!
   config.mysql = {
     client:  {
-      host: '47.101.222.13', //test
+      host: '47.101.222.13', //test 1
       port: '3306',
       user: 'admin',
       password: 'dMa4Ghgj', 
